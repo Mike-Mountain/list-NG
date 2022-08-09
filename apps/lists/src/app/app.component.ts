69fc3f4bcbe63407bd25e8422d617ef70642c4c8
Michael <mikemountain.dev@gmail.com>
2022-08-09 13:20:17 +0200

    Create the lists application

---
+++ apps/lists/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lst-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'lists';
+}
