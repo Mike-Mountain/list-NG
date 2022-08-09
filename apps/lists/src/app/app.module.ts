ad70cb1fdf36b73ea4e1c214dece3d5749c6fef1
Michael <mikemountain.dev@gmail.com>
2022-08-09 13:30:51 +0200

    Set up app

--- apps/lists/src/app/app.module.ts
+++ apps/lists/src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
