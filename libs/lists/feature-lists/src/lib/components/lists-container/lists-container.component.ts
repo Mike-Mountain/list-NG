c68ef65ed5d8ac0c1beedc7ce9006e2b1137d6e9
Michael Mountain <michael.mountain@ayo4u.com>
2022-08-11 10:03:37 +0200

    Add the list-container component

---
+++ libs/lists/feature-lists/src/lib/components/lists-container/lists-container.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lst-lists-container',
+  templateUrl: './lists-container.component.html',
+  styleUrls: ['./lists-container.component.scss'],
+})
+export class ListsContainerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- libs/lists/feature-lists/src/lib/feature-lists.module.ts
+++ libs/lists/feature-lists/src/lib/feature-lists.module.ts
@@ -8,6 +8,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
+import { ListsContainerComponent } from './components/lists-container/lists-container.component';
 
 @NgModule({
   imports: [
@@ -20,7 +21,7 @@ import { RouterModule } from '@angular/router';
     MatListModule,
     RouterModule,
   ],
-  declarations: [LayoutComponent],
+  declarations: [LayoutComponent, ListsContainerComponent],
   exports: [LayoutComponent],
 })
 export class FeatureListsModule {}
